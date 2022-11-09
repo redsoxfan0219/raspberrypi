427a112169bba8007900b89d520f4ccb32b7b681
Benjamin Moran <benjamin.moran.287@gmail.com>
2022-11-08 20:51:06 -0500

    First drafts of site, css, and api

--- lights-api.py
+++ lights-api.py
@@ -12,3 +12,21 @@ lights_pin = 6
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(6, GPIO.OUT)
 
+# Set up decorators
+
+@app.route("/")
+def index():
+    return render_template('lights.html')
+@app.route("/<deviceName>/")
+def action(deviceName):
+    if deviceName == 'lights':
+        relay = lights
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(relay, GPIO.OUT)
+        GPIO.output(relay, GPIO.LOW)
+        time.sleep(5)
+        GPIO.output(relay, GPIO.HIGH)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
